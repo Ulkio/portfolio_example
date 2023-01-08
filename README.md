ac245f949d11572a454b55014a3bb8aeb2827dfa
ulkio <guillaume.verschave@gmail.com>
2023-01-08 19:40:17 +0100

    dist folder

--- README.md
+++
@@ -1,3 +0,0 @@
-# portfolio_example
-
-Built with https://www.youtube.com/watch?v=JSJ8ftr92Vw
--- src/components/LineGradient.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const LineGradient = ({ width = "w-full" }) => {
-  return <div className={`h-0.5 ${width} bg-gradient-rainblue`}></div>;
-};
-
-export default LineGradient;
