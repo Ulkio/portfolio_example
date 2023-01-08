0a208c7cc0c61f413776c743e4c23cfb53a36793
ulkio <guillaume.verschave@gmail.com>
2023-01-08 19:33:43 +0100

    change base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/portfolio_example/",
+  base: "",
+  // base: "/portfolio_example/",
   plugins: [react()],
   resolve: {
     alias: {
